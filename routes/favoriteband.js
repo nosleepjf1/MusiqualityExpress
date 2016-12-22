32e7e71ead0f9c4df69c06dcc1855478b7dc05fd
Jonny Fewkes <jonnyfewkes@hotmail.com>
2016-12-22 10:33:53 -0700

    user login works

---
+++ routes/favoriteband.js
@@ -0,0 +1,17 @@
+var express = require('express');
+var router = express.Router();
+var UserFavoriteBand = require('../models/models.js').UserFavoriteBand;
+
+router.get('/:userid', function(req, res){
+
+});
+
+router.post('/save', function(req, res){
+
+});
+
+router.post('/delete', function(req, res){
+
+});
+
+module.exports = router;
